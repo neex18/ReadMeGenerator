1856920ed325ef8de6ca22161e25c11e11bbfa11
Nikko <88990996+neex18@users.noreply.github.com>
2021-11-13 10:48:53 -0800

    added to generatemarkdown

--- generateMarkDown.js
+++ generateMarkDown.js
@@ -25,7 +25,8 @@ function renderLicenseSection(license) {
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}, ${data.description}
+  return `# ${data.title}, ${data.description}, ${data.installation}, ${data.test},
+            ${data.report}, ${data.contribute}
   
 ${renderLicenseBadge(data.license)}
 
